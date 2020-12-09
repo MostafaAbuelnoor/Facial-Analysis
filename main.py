6ed791ab6163254310933abdf44a9d7447dec954
Mostafa Abuelnoor <mostafasaher@gmail.com>
2020-12-09 20:56:36 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from deepface import DeepFace
+# obj = DeepFace.analyze("jackie.jpg", actions = ['age', 'gender', 'race', 'emotion'])
+# #objs = DeepFace.analyze(["img1.jpg", "img2.jpg", "img3.jpg"]) #analyzing multiple faces same time
+# print(obj["age"]," years old ",obj["dominant_race"]," ",obj["dominant_emotion"]," ", obj["gender"])
+
+
+
+# result  = DeepFace.verify("will.jpg", "cropped.jpg")
+# #results = DeepFace.verify([['img1.jpg', 'img2.jpg'], ['img1.jpg', 'img3.jpg']])
+# print("Is verified: ", result["verified"])
+
+
+# DeepFace.stream("database")
